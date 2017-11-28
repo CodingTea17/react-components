7570035bb59f6de95830f0c03038367cd7a93c04
Dawson M <dawson.mortenson@gmail.com>
2017-11-28 14:51:35 -0800

    explore the basic usage of classes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,39 @@
-"use strict";
+'use strict';
 
-// JSX - JavaScript XML
-console.log("App.js is running");
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var app = {};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var render = function render() {
-  var template = React.createElement("div", null);
-  ReactDOM.render(template, document.getElementById('app'));
-};
+var Person = function () {
+  // Gives the constructor to have a default name if one is not given
+  function Person() {
+    var args = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { name: 'Prof. X', age: 100 };
 
-render();
+    _classCallCheck(this, Person);
+
+    this.name = args.name;
+    this.age = args.age;
+  }
+
+  _createClass(Person, [{
+    key: 'getGreeting',
+    value: function getGreeting() {
+      return 'Hi. I am ' + this.name + '.';
+    }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      return this.name + ' is ' + this.age + ' years old.';
+    }
+  }]);
+
+  return Person;
+}();
+
+var me = new Person({ name: 'Dawson Mortenson', age: 22 });
+console.log(me);
+console.log(me.getGreeting());
+console.log(me.getDescription());
+
+var x = new Person();
+console.log(x);
--- src/es6-classes.js
+++ src/es6-classes.js
@@ -1,5 +1,24 @@
 class Person {
+  // Gives the constructor to have a default name if one is not given
+  constructor(args = {name: 'Prof. X', age: 100}) {
+    this.name = args.name
+    this.age = args.age
+  }
 
+  getGreeting() {
+    return `Hi. I am ${this.name}.`;
+  }
+
+  getDescription() {
+    return `${this.name} is ${this.age} years old.`
+  }
 }
 
-const me = new Person();
+const me = new Person({ name: 'Dawson Mortenson', age: 22});
+console.log(me);
+console.log(me.getGreeting());
+console.log(me.getDescription());
+
+
+const x = new Person();
+console.log(x);
